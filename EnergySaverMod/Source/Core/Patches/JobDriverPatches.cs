9e20a9323d152c10550081f8523b76e162bfcb2d
ilkeraktug <63074357+ilkeraktug@users.noreply.github.com>
2025-08-13 01:38:23 +0300

    end job bug fix

---
+++ EnergySaverMod/Source/Core/Patches/JobDriverPatches.cs
@@ -0,0 +1,41 @@
+﻿using EnergySaverMod.Source.Core.Container;
+using EnergySaverMod.Source.Core.Helper;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace EnergySaverMod.Source.Core.Patches;
+
+[HarmonyPatch(typeof(JobDriver), "Cleanup")]
+public static class JobDriver_Cleanup_Patches
+{
+	public static void Postfix(JobDriver __instance, JobCondition condition)
+	{
+		if (__instance?.job != null && 
+		    (__instance.job.def == JobDefOf.DoBill || __instance.job.def == JobDefOf.Research))
+		{
+			Thing targetThing = __instance.job.targetA.Thing;
+			CompFlickable flickable = GetComponentHelper.GetFlickableComponent(targetThing);
+            
+			if(flickable != null && PatchesHelper.ShouldSwitchPower(targetThing))
+			{
+				PatchesHelper.SetSwitch(flickable, false);
+			}
+            
+			FacilityHelper.SetLinkedMultiAnalyzerSwitch(targetThing as Building_ResearchBench, false);
+		}
+		else if (__instance?.job?.def == JobDefOf.AnalyzeItem)
+		{
+			Thing targetThing = __instance.job.targetB.Thing;
+			CompFlickable flickable = GetComponentHelper.GetFlickableComponent(targetThing);
+            
+			if(flickable != null && PatchesHelper.ShouldSwitchPower(targetThing))
+			{
+				PatchesHelper.SetSwitch(flickable, false);
+			}
+            
+			FacilityHelper.SetLinkedMultiAnalyzerSwitch(targetThing as Building_ResearchBench, false);
+		}
+	}
+}
